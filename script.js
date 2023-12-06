949a81e0e1742c09385c1bf4b13a5041d7616faa
mytin-design <denmytin@gmail.com>
2023-12-06 15:15:32 +0300

    buildingprojects

---
+++ script.js
@@ -0,0 +1,10 @@
+let editt1 = document.getElementById("webT");
+
+let cwebt1 = document.getElementById("webTitle");
+
+function changeMainTitle() {
+    cwebt1.innerHTML = editt1.value;
+}
+
+//Javascripts -seems to not be a middle ware technology
+//changes effected in one document are not reflected in another;
